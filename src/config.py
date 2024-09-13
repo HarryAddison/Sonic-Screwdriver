2dd39136b5c61b1c934d9abf4ac1fca56abd4d40
Harry <ha00871@surrey.ac.uk>
2024-09-13 15:25:30 +0100

    "Initial commit"

---
+++ src/config.py
@@ -0,0 +1,12 @@
+dalek_train_spec_path = "/home/ha00871/Downloads/daleknn1_data/grids/grid1_v2_log_uniform_fluxes_16feb20_part1_train_interp_98k.npy"
+dalek_train_spec_wave_path = "/home/ha00871/Downloads/daleknn1_data/grids/wavelength.npy"
+dalek_train_params_path = "/home/ha00871/Downloads/daleknn1_data/grids/grid1_v2_log_uniform_params_16feb20_part1_train_98k.h5"
+dalek_network_dir = "/home/ha00871/Downloads/daleknn1_data/networks"
+dalek_network_fns = ["00-260285.h5", "01-260022.h5", "02-260627.h5", "03-261931.h5", "04-261295.h5"]
+
+
+instruments_dir = "/home/ha00871/PhD/Outreach/sn-spec-sonification/samples"
+
+plot_path = "/home/ha00871/PhD/Outreach/sn-spec-sonification/output/spectrum.PNG"
+
+audio_path = "/home/ha00871/PhD/Outreach/sn-spec-sonification/output/spectrum.wav"
